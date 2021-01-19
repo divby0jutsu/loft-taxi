a82aa9a7e466c29755e958a97fb2a24f19196af0
Dine Mashlenko <outofdelorean@gmail.com>
2021-01-18 23:32:30 -0800

    feat(fonts and forms styling added): fonts added, forms styling added
    Login and register forms css modules added, Roboto font added

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from'./Login.module.css';
 
 
 class Login extends React.Component {
@@ -9,14 +10,15 @@ render(){
     this.props.navigateTo(type);
   }
   return (
-    <form onSubmit={(e) => switchPage(e, 'map')}>
+    <form className={styles.Login} onSubmit={(e) => switchPage(e, 'map')}>
       <h1>Войти</h1>
       <label htmlFor="email">Email</label>
-      <input type="email" name="email" id="email" size="28"/>
+      <input type="email" name="email" id="email" size="28" placeholder="mail@mail.ru"/>
       <label htmlFor="password">Пароль</label>
-      <input type="password" name="password" id="password" size="28"/>
-      <button type="submit">Войти</button>
-      <p>Новый пользователь? <button onClick={(e) => switchPage(e, 'register')}>Регистрация</button></p>
+      <input type="password" name="password" id="password" size="28" placeholder="*************"/>
+      <a>Забыли пароль?</a>
+      <button className={styles.Submitbutton} type="submit">Войти</button>
+      <p>Новый пользователь? <button className={styles.Registerbutton} onClick={(e) => switchPage(e, 'register')}>Регистрация</button></p>
     </form>
   );
 }
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from './Register.module.css';
 
 class Register extends React.Component {
   render() {
@@ -7,16 +8,16 @@ class Register extends React.Component {
       this.props.navigateTo(type);
     }
   return (
-    <form onSubmit={(e) => switchPage(e, 'map')}>
+    <form className={styles.Register} onSubmit={(e) => switchPage(e, 'map')}>
       <h1>Регистрация</h1>
       <label htmlFor="email">Email*</label>
       <input type="email" name="email" id="email" size="28" placeholder="mail@mail.ru"/>
       <label htmlFor="name">Как вас зовут?*</label>
       <input type="text" name="name" id="name" size="38" placeholder="Петр Александрович"/>
       <label htmlFor="password">Придумайте пароль*</label>
-      <input type="password" name="password" id="password" size="28"/>
-      <button type="submit">Зарегистрироваться</button>
-      <p>Уже зарегистрированы? <button onClick={(e) => switchPage(e, 'login')}>Войти</button></p>
+      <input type="password" name="password" id="password" size="28" placeholder="*************"/>
+      <button  className={styles.Submitbutton} type="submit">Зарегистрироваться</button>
+      <p>Уже зарегистрированы? <button className={styles.Loginbutton} onClick={(e) => switchPage(e, 'login')}>Войти</button></p>
     </form>
   );
   }
